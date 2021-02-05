097f44da7e7d003de83dd69ce62f3e258d870bef
Dyatlov Andrey <fantazey@gmail.com>
2021-02-05 21:40:39 +0300

    * update config + components and entry point

---
+++ src/components/HelloWorld/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => (
+    <>
+        <h1>Hello World</h1>
+        <hr/>
+        <h3>Test react/ts application</h3>
+    </>
+);
+
+export default HelloWorld
